5b381b58de9ac25ae5d3a97f862e59c7c126507a
Dongyi <dywang@terpmail.umd.edu>
2017-06-18 21:05:32 -0400

    POJ1000

---
+++ C1Introduction/POJ1000.cpp
@@ -0,0 +1,27 @@
+//POJ1000 A+B problem
+#include<iostream>
+#include<string>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	int a=0, b=0;
+	long sum;
+	try
+	{
+		a = stoi(argv[1]);
+		b = stoi(argv[2]);
+	}
+	catch(...)
+	{
+		cerr << "wrong parameter types";
+		return 0;
+	}
+	if((a>=0)&&(a<=10)&&(b>=0)&&(b<=10))
+	{
+		sum = a+b;
+		cout << "the output is:"<<sum<<endl;
+	}
+	else
+		cout << "the input number is not in the acceptable range";
+	return 0;
+}
